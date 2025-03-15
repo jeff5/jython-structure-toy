eed945a2a2c522079d41a7a2d61baa1068e26f42
Jeff Allen <ja.py@farowl.co.uk>
2025-03-15 10:05:54 +0000

    Extract MyType to show Application only needs core package

--- app/src/main/java/example/app/Application.java
+++ app/src/main/java/example/app/Application.java
@@ -1,13 +1,9 @@
 package example.app;
 
-import java.lang.invoke.MethodHandles;
-
 import example.core.Interp;
 import example.extension.Extension;
-import example.runtime.Exposed;
-import example.runtime.PyType;
-import example.runtime.PyUtil;
 
+/** An application written using only core classes. */
 public class Application {
 
     public static void main(String[] args) {
@@ -51,28 +47,5 @@ public class Application {
         System.out.println(mt);
         interp.callMethod(mt, "set_content", 21);
         System.out.println(mt);
-    }
-
-    static class MyType {
-
-        private int content;
-
-        MyType(int content) { this.content = content; }
-
-        @Exposed.PythonMethod
-        Object __str__() { return "MyType(" + content + ")"; }
-
-        @Exposed.PythonMethod
-        static void set_content(MyType self, int v) {
-            self.content = 2 * v;
-        }
-
-        @Override
-        public String toString() {
-            return PyUtil.defaultToString(this);
-        }
-
-        static final PyType TYPE = PyType.register("MyType",
-                MyType.class, MethodHandles.lookup());
     }
 }
---
+++ app/src/main/java/example/app/MyType.java
@@ -0,0 +1,32 @@
+package example.app;
+
+import java.lang.invoke.MethodHandles;
+
+import example.runtime.Exposed;
+import example.runtime.PyType;
+import example.runtime.PyUtil;
+
+/**
+ * A type used in the {@link Application} and defined with facilities in
+ * the {@code runtime} package.
+ */
+class MyType {
+
+    private int content;
+
+    MyType(int content) { this.content = content; }
+
+    @Exposed.PythonMethod
+    Object __str__() { return "MyType(" + content + ")"; }
+
+    @Exposed.PythonMethod
+    static void set_content(MyType self, int v) {
+        self.content = 2 * v;
+    }
+
+    @Override
+    public String toString() { return PyUtil.defaultToString(this); }
+
+    static final PyType TYPE = PyType.register("MyType", MyType.class,
+            MethodHandles.lookup());
+}
